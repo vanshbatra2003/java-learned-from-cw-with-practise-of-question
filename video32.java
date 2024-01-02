890461b99c3f6954e40a4dab862cfbc7c1e38955
Vansh Batra <101448776+vanshbatra2003@users.noreply.github.com>
2024-01-02 23:09:04 +0530

    day-2

--- video32.java
+++ video32.java
@@ -1,14 +1,28 @@
 import java.util.*;
 public class video32 {
     static int factorial(int n){
-        if(n==1 || n==0){
+        if(n==0){
             return 1;
         }
-        return n*factorial(n-1);
+        // int small=factorial(n-1);
+        // int ans=n*small;
+        // return ans; 
+        // or
+        return(n*factorial(n-1));   
+    }
+    static int fibonacci(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return(fibonacci(n-1)+fibonacci(n-2));
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         System.out.println(factorial(n));
+        System.out.println(fibonacci(n));
     }    
 }
